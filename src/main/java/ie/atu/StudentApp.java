62fded20f6644b55278225906173f87c4b354401
Oryn <G00406032@atu.ie>
2022-10-24 10:54:04 +0100

    changes made to Student and studentapp

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class StudentApp
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //first instance
         System.out.println("Please enter your name: ");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
+        Student StdName = new Student();
         System.out.println("You entered: " + name + "\n");
+        StdName.setName(name);
 
         System.out.println("Please enter your student email: ");
         String email = input.nextLine();
+        Student StdEmail = new Student();
         System.out.println("You entered: " + email + "\n");
+        StdEmail.setName(email);
 
         System.out.println("Please enter your course name: ");
         String course = input.nextLine();
+        Student StdCourse = new Student();
         System.out.println("You entered: " + course + "\n");
+        StdCourse.setName(course);
     }
 }
