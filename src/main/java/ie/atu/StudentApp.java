6903fd35ee117c4101d7b47c910c4b5f6b46135c
Oryn <G00406032@atu.ie>
2022-10-28 14:09:26 +0100

    created second instance of getDetails

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -7,23 +7,40 @@ public class StudentApp
     public static void main(String[] args)
     {
         //first instance
-        System.out.println("Please enter your name: ");
+        System.out.println("Please enter the first name: ");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        Student StdName = new Student();
-        System.out.println("You entered: " + name + "\n");
-        StdName.setName(name);
+        Student Getdetails = new Student();
+        Getdetails.setName(name);
+        System.out.println("You entered: " + Getdetails.getName() + "\n");
 
-        System.out.println("Please enter your student email: ");
+        System.out.println("Please enter the first student email: ");
         String email = input.nextLine();
-        Student StdEmail = new Student();
-        System.out.println("You entered: " + email + "\n");
-        StdEmail.setName(email);
+        Getdetails.setEmail(email);
+        System.out.println("You entered: " + Getdetails.getEmail() + "\n");
 
-        System.out.println("Please enter your course name: ");
+        System.out.println("Please enter the first course name: ");
         String course = input.nextLine();
-        Student StdCourse = new Student();
-        System.out.println("You entered: " + course + "\n");
-        StdCourse.setName(course);
+        Getdetails.setCourse(course);
+        System.out.println("You entered: " + Getdetails.getCourse() + "\n");
+
+
+        //second instance
+        Student Getdetails2 = new Student();
+        System.out.println("Please enter the second name: ");
+        String name2 = input.nextLine();
+        Getdetails2.setName(name2);
+        System.out.println("You entered: " + Getdetails2.getName() + "\n");
+
+        System.out.println("Please enter the second student email: ");
+        String email2 = input.nextLine();
+        Getdetails2.setEmail(email2);
+        System.out.println("You entered: " + Getdetails2.getEmail() + "\n");
+
+        System.out.println("Please enter the second course name: ");
+        String course2 = input.nextLine();
+        Getdetails2.setCourse(course2);
+        System.out.println("You entered: " + Getdetails2.getCourse() + "\n");
+
     }
 }
